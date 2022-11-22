a06309b2f322e43967eec3598533fcd93f24eea0
Shruti Ramenahalli <shrutir456@gmail.com>
2022-11-22 21:49:49 +0000

    Add files via upload

---
+++ Intro2CSPython/Week 3/Wednesday/rock_paper_scissors.py
@@ -0,0 +1,19 @@
+import rock_paper_scissors_helper as helper
+
+while (True):
+    player = input("Enter your choice (machine, user) : ")
+    if(player == "!"):
+        exit(1)
+
+    if(player != "machine") & (player !="user"):
+        print("You have entered incorrect option! Please chose from the options!")
+        player = input("Enter your choice (machine, user) : ")
+    else:
+        helper.RPS_Game(player)
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() == "y":
+        player = input("Enter your choice (machine, user) : ")
+        helper.RPS_Game(player)
+    else:
+        print("Thanks for playing!")
